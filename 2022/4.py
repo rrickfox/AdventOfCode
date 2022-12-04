12ff7086340fd657c76f1c452c84d58f10189343
Richard Fuchs <31240615+rrickfox@users.noreply.github.com>
2022-12-04 11:03:35 +0100

    Simplify part 2 day 4 2022

--- 2022/4.py
+++ 2022/4.py
@@ -10,7 +10,8 @@ value2 = 0
 for (elve1, elve2) in data:
     if (elve1[0] <= elve2[0] and elve1[1] >= elve2[1]) or (elve2[0] <= elve1[0] and elve2[1] >= elve1[1]):
         value1 += 1
-    if (elve1[0] >= elve2[0] and elve1[0] <= elve2[1]) or(elve1[1] >= elve2[0] and elve1[1] <= elve2[1]) or (elve2[0] >= elve1[0] and elve2[0] <= elve1[1]) or(elve2[1] >= elve1[0] and elve2[1] <= elve1[1]):
+    # if (elve1[0] >= elve2[0] and elve1[0] <= elve2[1]) or(elve1[1] >= elve2[0] and elve1[1] <= elve2[1]) or (elve2[0] >= elve1[0] and elve2[0] <= elve1[1]) or(elve2[1] >= elve1[0] and elve2[1] <= elve1[1]):
+    if elve1[1] >= elve2[0] and elve2[1] >= elve1[0]:
         value2 += 1
 
 print(value1)
