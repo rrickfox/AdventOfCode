ce9cdfaf3db86ce3b1f2072038c595ce8f7dca84
Richard Fuchs <31240615+rrickfox@users.noreply.github.com>
2022-12-19 22:25:47 +0100

    Use variables instead of tuples and use proper deque instead of list

--- 2022/19.py
+++ 2022/19.py
@@ -1,68 +1,55 @@
 import time, parse
+from collections import deque
 start_time = time.time()
 
 p = parse.compile("Blueprint {}: Each ore robot costs {} ore. Each clay robot costs {} ore. Each obsidian robot costs {} ore and {} clay. Each geode robot costs {} ore and {} obsidian.")
 
 with open("19.txt", "r") as file:
     lines = [list(map(int, p.parse(line.strip()))) for line in file.readlines()]
-    data = [[[line[1], 0, 0], [line[2], 0, 0], [line[3], line[4], 0], [line[5], 0, line[6]]] for line in lines]
-
-def sum_vector(a, b, neg = False):
-    if neg: return [sum(x) for x in zip(a,(-x for x in b))]
-    return [sum(x) for x in zip(a,b)]
+    data = [[line[1], line[2], (line[3], line[4]), (line[5], line[6])] for line in lines]
 
 def calc(rob, cost, res, time_start):
-    maxes = [max(x) for x in zip(*cost)]
     done = set()
-    todo = [(rob, res, time_start)]
+    todo = deque([(*rob, *res, time_start)])
     best_geode = 0
-    while len(todo):
-        robots, resources, remaining_time = todo.pop(0)
+    r1cost, r2cost, (r3costOre, r3costClay), (r4costOre, r4costObs) = cost
+    maxOre = max([r1cost, r2cost, r3costOre, r4costOre])
+    while todo:
+        r1, r2, r3, r4, res1, res2, res3, res4, remaining_time = todo.popleft()
 
-        # best_geode = max(best_geode, resources[-1])
-        if resources[-1] > best_geode:
-            best_geode = resources[-1]
-            # print(f"new best: {best_geode}")
-        if remaining_time == 0:
+        best_geode = max(best_geode, res4)
+
+        if remaining_time == 0 or best_geode > res4 + remaining_time*r4 + (remaining_time*(remaining_time-1))/2:
             continue
 
-        # clamp robots (no need for 5 ore robots if we can only spend 4 ore per minute)
-        # robots = list(robots)
-        # for i in range(3):
-        #     if robots[i] > maxes[i]:
-        #         robots[i] = maxes[i]
-        # robots = tuple(robots)
 
         # clamp resources (if we have 5 robots, 10 minutes left and can spend 10 per minute, the maximum number of resources we need now is 45 (10*10)-(9*5))
-        resources = list(resources)
-        if resources[0] >= remaining_time * maxes[0] - (remaining_time - 1) * robots[0]:
-            resources[0] = remaining_time * maxes[0] - (remaining_time - 1) * robots[0]
-        if resources[1] >= remaining_time * maxes[1] - (remaining_time - 1) * robots[1]:
-            resources[1] = remaining_time * maxes[1] - (remaining_time - 1) * robots[1]
-        if resources[2] >= remaining_time * maxes[2] - (remaining_time - 1) * robots[2]:
-            resources[2] = remaining_time * maxes[2] - (remaining_time - 1) * robots[2]
-        resources = tuple(resources)
+        if res1 >= remaining_time * maxOre - (remaining_time - 1) * r1:
+            res1 = remaining_time * maxOre - (remaining_time - 1) * r1
+        if res2 >= remaining_time * r3costClay - (remaining_time - 1) * r2:
+            res2 = remaining_time * r3costClay - (remaining_time - 1) * r2
+        if res3 >= remaining_time * r4costObs - (remaining_time - 1) * r3:
+            res3 = remaining_time * r4costObs - (remaining_time - 1) * r3
         
-        if (robots, resources, remaining_time) in done:
+        if (r1, r2, r3, r4, res1, res2, res3, res4, remaining_time) in done:
             continue
-        done.add((robots, resources, remaining_time))
+        done.add((r1, r2, r3, r4, res1, res2, res3, res4, remaining_time))
         
-        if len(done) % 1000000 == 0:
-            print(f"t: {remaining_time}, best: {best_geode}, done: {len(done)}")
-            print("--- %s seconds ---" % (time.time() - start_time))
-        
-        building = []
-        for robot_type, c in enumerate(cost):
-            if all(available >= needed for available, needed in zip(resources, c)):
-                building.append(robot_type)
-        
-        resources = tuple(sum_vector(resources, robots))
-        todo.append((robots, resources, remaining_time - 1))
-        for robot in building:
-            if robot == 3 or robots[robot] < maxes[robot]:
-                t = list(robots)
-                t[robot] += 1
-                todo.append((tuple(t), tuple(sum_vector(resources, cost[robot] + [0], True)), remaining_time - 1))
+        # if len(done) % 1000000 == 0:
+        #     print(f"t: {remaining_time}, best: {best_geode}, done: {len(done)}")
+        #     print("--- %s seconds ---" % (time.time() - start_time))
+
+        todo.append((r1, r2, r3, r4, res1 + r1, res2 + r2, res3 + r3, res4 + r4, remaining_time - 1))
+
+        # buy and also clamp robots (no need for 5 ore robots if we can only spend 4 ore per minute)
+        if res1 >= r1cost and r1 < maxOre:
+            todo.append((r1 + 1, r2, r3, r4, res1 + r1 - r1cost, res2 + r2, res3 + r3, res4 + r4, remaining_time - 1))
+        if res1 >= r2cost and r2 < r3costClay:
+            todo.append((r1, r2 + 1, r3, r4, res1 + r1 - r2cost, res2 + r2, res3 + r3, res4 + r4, remaining_time - 1))
+        if res1 >= r3costOre and res2 >= r3costClay and r3 < r4costObs:
+            todo.append((r1, r2, r3 + 1, r4, res1 + r1 - r3costOre, res2 + r2 - r3costClay, res3 + r3, res4 + r4, remaining_time - 1))
+        if res1 >= r4costOre and res3 >= r4costObs:
+            todo.append((r1, r2, r3, r4 + 1, res1 + r1 - r4costOre, res2 + r2, res3 + r3 - r4costObs, res4 + r4, remaining_time - 1))
 
     return best_geode
 
