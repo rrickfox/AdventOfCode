316ad48a0c9eead89d2d79e120b6c9e651f7847f
Richard Fuchs <31240615+rrickfox@users.noreply.github.com>
2023-12-05 19:49:06 +0100

    Add day 4 golf

--- 2023/04.py
+++ 2023/04.py
@@ -1,10 +1,15 @@
 import time, re
+import parse as p
 start_time = time.time()
 
 with open("04.txt", "r") as file:
     lines = [line.strip() for line in file.readlines()]
-    data = [len(set(l[0].split(" ")).intersection(set(l[1].split(" ")))) for line in lines if (l := re.sub("\s+", " ", line.split(": ")[1]).split(" | "))]
-print(sum([2**(p-1) for p in data if p > 0]))
+    # data = [len(set(l[0].split(" ")).intersection(set(l[1].split(" ")))) for line in lines if (l := re.sub("\s+", " ", line.split(": ")[1]).split(" | "))]
+# print(sum([2**(p-1) for p in data if p > 0]))
+# data = [len(set(l[0].split(" ")).intersection(set(l[1].split(" ")))) for line in open("04.txt", "r").readlines() if (l := re.sub("\s+", " ", line.strip().split(": ")[1]).split(" | "))]
+data = [len(set(l[1].split()).intersection(set(l[2].split()))) for line in open("04.txt","r").readlines() if (l:=p.parse("Card {}: {} | {}\n", line))]
+data = [sum(1 for x in l[1].split() if x in l[2].split()) for line in open("04.txt","r").readlines() if (l:=p.parse("Card {}: {} | {}\n", line))]
+print(sum([2**(p-1) for p in data if p]))
 
 num_cards = {}
 res = 0
